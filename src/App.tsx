d8d867e9ad1cf75989ee00b79730a0ee216c0cd4
koh-yoshimoto <vagrant@vagrant.vm>
2021-05-04 09:01:24 +0000

    Add TODO Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React, { useState } from 'react'
+import { Task } from './components/Types'
+import TaskInput from './components/TaskInput'
+import TaskList from './components/TaskList'
 import './App.css'
 
+const initialState: Task[] = [
+  {
+    id: 2,
+    title: '次にやる',
+    done: false
+  },{
+    id: 1,
+    title: '最初にやる',
+    done: true
+  }
+]
+
 const App: React.FC = () => {
-    return (
-      <p>Hello World</p>
-    )
+  const [tasks, setTasks] = useState(initialState)
+
+  return (
+    <div>
+      <TaskInput setTasks={setTasks} tasks={tasks} />
+      <TaskList setTasks={setTasks} tasks={tasks} />
+    </div>
+  )
+
 }
 
 export default App
